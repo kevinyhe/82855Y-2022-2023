5259824f1b3ae3fe71822fa1b1350ed948a7562e
Kerry Li <kerry4sports@gmail.com>
2022-12-07 18:37:03 -0500

    Kerrys commit, changes to main.cpp, not much differnt

--- src/main.cpp
+++ src/main.cpp
@@ -43,13 +43,26 @@ void disabled() {}
 void competition_initialize() {}
 
 void autonomous() {
+	left_front.move_absolute(100, 100); // Moves 100 units forward
+  	while (!((left_front.get_position() < 105) && (left_front.get_position() > 95))) {
+    // Continue running this loop as long as the motor is not within +-5 units of its goal
+ 	   pros::delay(2);
+	}
+	left_front.move_relative(100, 100); // Also moves 100 units forward
+  	while (!((left_front.get_position() < 205) && (left_front.get_position() > 195))) {
+    	pros::delay(2);
+  }
 	// left_front.move_absolute(100, 200);
 	// pros::delay(1000);
 }
 
 void opcontrol() {
-	int drive_power = 127;
-	int turn_power = 50;
+	
+
+
+	
+	int drive_power = 90;
+	int turn_power = 40;
 
 	while (true) {
 		/**
@@ -67,5 +80,22 @@ void opcontrol() {
 		right_back.move(drive_power - turn_power);
 
 		pros::delay(20);
+
+
+	    left_front = master.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
+    	std::cout << "Left Front Motor Position: " << left_front.get_position();
+		left_middle = master.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
+    	std::cout << "Left Middle Position: " << left_middle.get_position();
+		left_back = master.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
+    	std::cout << "Left Back Motor Position: " << left_back.get_position();
+		right_front = master.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
+    	std::cout << "Right Front Motor Position: " << right_front.get_position();
+		right_middle = master.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
+    	std::cout << "Right Middle Motor Position: " << right_middle.get_position();
+		right_back = master.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
+    	std::cout << "Right Back Motor Position: " << right_back.get_position();
+
+    	pros::delay(2);
+		
 	}
 }
